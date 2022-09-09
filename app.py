ec852eee6863323095b62c8bab3365a52e3156a7
santiave <98056870+santiave@users.noreply.github.com>
2022-09-09 19:24:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,15 @@ if img is not None:
     result=ocr.ocr(img)
     st.write(f'VIN: {"".join(i for i in result[0][1][0] if (i.isascii() and i.isalnum())) }')
     st.write(f'ACCURACY: {result[0][1][1]:.1%}')
+    (tl, tr, br, bl) = result[0][0]
+    tl = (int(tl[0]), int(tl[1]))
+    tr = (int(tr[0]), int(tr[1]))
+    br = (int(br[0]), int(br[1]))
+    bl = (int(bl[0]), int(bl[1]))
+    cv2.line(img, tl, tr, (0, 255, 0), 10)
+    cv2.line(img, tr, br, (0, 255, 0), 10)
+    cv2.line(img, br, bl, (0, 255, 0), 10)
+    cv2.line(img, bl, tl, (0, 255, 0), 10)
+    st.image(img)
     
   
